847e9431b63e1fcf13eebed6228b680fd2ae7d1a
Rakesh Ranjan <rakesh5.ranjan@aricent.com>
2022-03-13 00:12:35 +0530

    Base structure setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col>Dummy</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
